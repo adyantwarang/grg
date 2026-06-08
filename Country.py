da7282321708948e8cd7cfe6c623d46b2afaf22d
adyantwarang <waranganmol@gmail.com>
2026-06-08 21:06:21 +0530

    Add files via upload

---
+++ Country.py
@@ -0,0 +1,9 @@
+cont = {'Poland':'9302',
+        'Uruguay':'5413',
+        'Palestine':'8760'}
+
+print("Code for Poland - ")
+print(cont.get('Poland', 'Not found'))
+
+print("Code for Egypt - ")
+print(cont.get('Egypt', 'Not found'))
